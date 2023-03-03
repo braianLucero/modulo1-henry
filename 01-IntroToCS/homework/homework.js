792f7adb9d93b2aa96c1781d6c8e4cc40df579c4
brian Lucero <brasanluc123@gmail.com>
2023-03-03 01:04:11 -0300

    work-binario-decimal

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,22 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   let sum  = 0;
 
-function DecimalABinario(num) {}
+   for (let i = 0; i < num.length;i++){
+      sum  = sum + num[i] * 2 ** (num.length - 1 - i);
+   }
+   return sum
+}
+
+function DecimalABinario(num) {
+   let  binary  = '';
+   while (num > 0){
+         binary = (num % 2) + binary;
+         num = Math.floor(num / 2);      
+   }
+   return binary;
+}
 
 module.exports = {
    BinarioADecimal,
