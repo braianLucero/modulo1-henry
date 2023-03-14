5833bf8266ed2ea666e6154027e0a80a6d21f9c1
brian Lucero <brasanluc123@gmail.com>
2023-03-13 23:26:02 -0300

    homework8-ultima

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -6,8 +6,39 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+   if(array.length <= 1 )return array;
+   let pivot = array[0];
+   let left = [];
+   let right = [];
 
+   for (let i = 1; i < array.length; i++) {
+    if(array[i] > pivot){
+      right.push(array[i])
+    }else{
+      left.push(array[i])
+    }
+   }
+   return quickSort(left).concat(pivot).concat(quickSort(right));
 }
+
+
+function merge(left,right){
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  let arrOrdered = [];
+  while(leftIndex < left.length && rightIndex < right.length){
+    if(left[leftIndex] <  right[rightIndex]){
+      arrOrdered.push(left[leftIndex]);
+      leftIndex++;
+
+    }else{
+      arrOrdered.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+  return arrOrdered.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+ }
 
 function mergeSort(array) {
   // Implementar el método conocido como mergeSort para ordenar de menor a mayor
@@ -16,6 +47,14 @@ function mergeSort(array) {
   // Tu código:
 
   //okey probemos ahora
+  if(array.length === 1 )return array;
+
+  let mid = Math.floor(array.length / 2 );
+  let left = array.slice(0, mid);
+  let right = array.slice(mid); 
+
+  return merge(mergeSort(left), mergeSort(right))
+   
 }
 
 // No modificar nada debajo de esta línea
